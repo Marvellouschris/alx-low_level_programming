c3f12971ef2b01f1c5bf6453cdf621ba330197d2
Marvellouschris <marvellousekhatorik@gmail.com>
2022-06-30 15:23:24 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
- * main - 
- * return 0
- */
+ * main - main block
+ * Return: 0
+ */  
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - prints the size of various types
- * return; 0 if exited properly, non-zero otherwise
+ * main - block
+ * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n"' sizeof(float));
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longlongType;
+	float floatType;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
-}
