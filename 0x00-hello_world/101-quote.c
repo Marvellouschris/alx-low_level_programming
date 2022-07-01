88e5e4576259d2482cbb4a788e00554f9f0d5ce3
Marvellouschris <marvellousekhatorik@gmail.com>
2022-06-30 17:19:23 -0700

    unix is basically an operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - print exactly "and that piece of art is useful"
  * return: 1
@@ -8,6 +6,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
