05468d1b8d013becb9e0e86d6f13b32f17a636a2
Marvellouschris <marvellousekhatorik@gmail.com>
2022-07-01 23:54:01 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
-  * main -main block to e executed
-  * Descripton: program that prints all possible combinations of digits
-  * Return: 0
-  */
-
-int main(void)
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
 {
-int c = 0;
-int f_d;
-int l_d;
-
-int c2;
-int f_d2;
-int l_d2;
-
-while (c <= 98)
+int num1, num2;
+for (num1 = 0; num1 <= 98; num1++)
 {
-f_d = (c / 10 + '0');
-l_d = (c % 10 + '0');
-c2 = 0;
-while (c2 <= 99)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-f_d2 = (c2 / 10 + '0');
-l_d2 = (c2 % 10 + '0');
-if (c<c2)
-{
-putchar(f_d);
-putchar(l_d);
-putchar(' ');
-putchar(f_d2);
-putchar(l_d2);
-if (c!=98)
-{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 == 98 && num2 == 99)
+continue;
 putchar(',');
-putchar(' ');
+putchar(' ');
 }
-}
-c2++;
-}
-c++;
 }
 putchar('\n');
-return (0);
+return (0);
 }
