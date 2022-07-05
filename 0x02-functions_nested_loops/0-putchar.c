e094badfd77c6d9b037c10daed259863b6f5539d
Marvellouschris <marvellousekhatorik@gmail.com>
2022-07-05 09:23:07 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int main(void)
+
+/**
+ * main - printing _putchar
+ * return - 0
+ */	
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02-functions_nested_loops
+
+a program that prints _putchar, followed by a new line
