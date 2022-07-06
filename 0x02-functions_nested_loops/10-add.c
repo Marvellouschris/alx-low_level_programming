b17b6f445e9340003d8653eac3b5bbc6b81eeec4
Ekhator Marvellous <marvellousekhatorik@gmail.com>
2022-07-06 10:59:53 +0100

    saeve

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: knt data tyoe
+ * @num2: int data type
+ * Description: give two integers, returns sum
+ * Return: result
+ */
+
+int add(int num1, int num2)
+{
+    int result;
+
+	result = num1 + num2;
+	return (result);
+}
