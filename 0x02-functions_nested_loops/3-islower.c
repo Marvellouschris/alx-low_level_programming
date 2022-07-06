4def409af01078400eaced268818f8d9f5eca998
Ekhator Marvellous <marvellousekhatorik@gmail.com>
2022-07-06 15:04:05 +0100

    Alphabet character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * _islower - checks for lowercase
@@ -17,23 +16,4 @@ int _islower(int c)
 		return (0);
 	}
 }
-=======
-#include "main.h"
-/**
- * _islower - checks for lowercase
- * description - return intetger
- * @c: c  intger
- * Return: 1 if lowercase
-*/
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
->>>>>>> 923dd04aaa70ad992c6baf431235c6a0164734db
+
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
@@ -20,26 +19,3 @@ int _isalpha(int c)
 		return (0);
 	}
 }
-=======
-#include "main.h"
-/**
- * _isalpha - checks for alphabetic character
- *@c: is char type
- * Return: 1 (if letter)
-*/
-int _isalpha(int c)
-{
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
->>>>>>> 923dd04aaa70ad992c6baf431235c6a0164734db
