1b7fa7f847686f891e9d342d55ca54ce7b3a9701
Marvellouschris <marvellousekhatorik@gmail.com>
2022-07-07 18:54:57 -0700

    functioned nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,5 +18,6 @@ void print_number(int n)
 		x = n;
 	if (x / 10)
 		print_number(x / 10);
+
 	_putchar((x % 10) + '0');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -20,5 +20,6 @@ void print_most_numbers(void)
 			_putchar('0' + a);
 		}
 	}
+
 	_putchar('\n');
 }
