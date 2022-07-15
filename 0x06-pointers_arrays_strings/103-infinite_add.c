37a1b7703ff03b27b8e6e8b91ba9c6ad90dda4a0
Ekhator Marvellous <marvellousekhatorik@gmail.com>
2022-07-15 02:16:13 -0700

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,55 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * infinite_add -   a function that adds two numbers.
- * @n1: first parameter.
- * @n2: second parameter.
- * @r: third parameter.
- * @size_r: fourth parameter
- * Return: 0
- */
+ * infinite_add - add two numbers
+ * Description - a function that adds two numbers
+ * @n1: the first number
+ * @n2: second number
+ * @r: buffer that the function will use to store result
+ * @size_r: buffer size
+ * Return: a pointer to the result
+*/
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+	int b = 0, c = 0, d = 0, e = 0, f, g, h;
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i > j)
-		l = i;
+	while (n1[b] != '\0')
+		b++;
+	while (n2[c] != '\0')
+		c++;
+	if (b > c)
+		d = b;
 	else
-		l = j;
-	if (l + 1 > size_r)
+		d = c;
+	if (d + 1 > size_r)
 		return (0);
-	r[l] = '\0';
-	for (k = l - 1 ; k >= 0 ; k--)
+
+	r[d] = '\0';
+
+	for (f = d - 1; f >= 0; f--)
 	{
-		i--;
-		j--;
-		if (i >= 0)
-			f = n1[i] - '0';
+		b--;
+		c--;
+		if (b >= 0)
+			g = n1[b] - '0';
 		else
-			f = 0;
-		if (j >= 0)
-			s = n2[j] - '0';
+			g = 0;
+		if (c >= 0)
+			h = n2[c] - '0';
 		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
+			h = 0;
+		r[f] = (g + h + e) % 10 + '0';
+		e = (g + h + e) / 10;
 	}
-	if (d == 1)
+	if (e == 1)
 	{
-		r[l + 1] = '\0';
-		if (l + 2 > size_r)
+		r[d + 1] = '\0';
+		if (d + 2 > size_r)
 			return (0);
-		while (l-- >= 0)
-			r[l + 1] = r[l];
-		r[0] = d + '0';
+		while (d-- >= 0)
+			r[d + 1] = r[d];
+		r[0] = e + '0';
 	}
 	return (r);
 }
