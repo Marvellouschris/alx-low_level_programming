2e62af481583a009d15934cd6640f87bc06acf0d
Marvellouschris <marvellousekhatorik@gmail.com>
2022-07-20 15:10:01 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,4 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _putchar(char c);
